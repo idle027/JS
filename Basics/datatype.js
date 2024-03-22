401dd13480434e931634c85a1b69ad2e5fb4d489
idle027 <119083819+idle027@users.noreply.github.com>
2024-03-22 23:17:00 +0000

    datatypes

---
+++ Basics/datatype.js
@@ -0,0 +1,18 @@
+"use strict";  // treat all js code as newer version
+// alert("Hello") // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique value
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
