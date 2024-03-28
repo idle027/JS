eed19edeb364a8b80c58dcf3edf3077fab5e0c20
idle027 <119083819+idle027@users.noreply.github.com>
2024-03-28 22:25:17 +0000

    conversionOperation.js

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,63 @@
+let score = "Jim"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "37" => 37
+// "37abc" => NaN
+// true = 1 and false = 0
+
+let isLoggedIn = "Jim"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => "true"; 0 => "false"
+// " " => false
+// "jim" => true
+
+let someNumber = 37
+
+let stringNumber = String(someNumber)
+// console.log(typeof(stringNumber));
+
+// *********************** Operations ***********************
+
+let value = 7
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Jim"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("7" + 9);
+// console.log(7 + "9");
+// console.log("7" + 9 + 2);
+// console.log(7 + 9 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 1 + 1
+
+let gameCounter = 101
+++gameCounter;
+// console.log(gameCounter);
